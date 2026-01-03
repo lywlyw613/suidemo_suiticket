df9b9b3283bb5d913e1e1a41d77806c8f7bb8bf2
nictogin <2024havetobeok@gmail.com>
2026-01-03 08:10:09 +0800

    Fix models/index.ts export paths - add .js extensions

--- backend/src/models/index.ts
+++ backend/src/models/index.ts
@@ -1,11 +1,11 @@
 // Export all models from a single file
-export { User } from './User';
-export { Organizer } from './Organizer';
-export { Event } from './Event';
-export { TicketType } from './TicketType';
-export { Order } from './Order';
-export { OrderItem } from './OrderItem';
-export { NFTTicket } from './NFTTicket';
-export { TicketVerification } from './TicketVerification';
-export { Conversation } from './Conversation';
+export { User } from './User.js';
+export { Organizer } from './Organizer.js';
+export { Event } from './Event.js';
+export { TicketType } from './TicketType.js';
+export { Order } from './Order.js';
+export { OrderItem } from './OrderItem.js';
+export { NFTTicket } from './NFTTicket.js';
+export { TicketVerification } from './TicketVerification.js';
+export { Conversation } from './Conversation.js';
 
